d3046a369547ee1749353fce5c866c89664cc29e
MariKerdika <131894818+MariKerdika@users.noreply.github.com>
2023-10-10 14:47:39 +0400

    add cards section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../HeroSection";
 import HeroSection from "../HeroSection";
+import Cards from "../Cards";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 };

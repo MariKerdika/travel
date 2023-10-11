99615181fd2f6fcb97a921a6f45229d4916c1c42
MariKerdika <131894818+MariKerdika@users.noreply.github.com>
2023-10-11 18:52:39 +0400

    Second part of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "./Button";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -22,6 +23,96 @@ const Footer = () => {
             />
             <Button buttonStyle="btn--outline">Subscribe</Button>
           </form>
+        </div>
+      </section>
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+        </div>
+      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              trvl <i className="fab fa-typo3" />
+            </Link>
+          </div>
+          <small className="website-rights">trvl c 2020</small>
+          <div className="social-icons">
+            <Link
+              className="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              area-label="Facebook"
+            >
+              <i className="fab fa-facebook-f"></i>
+            </Link>
+            <Link
+              className="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              area-label="Instagram"
+            >
+              <i className="fab fa-instagram"></i>
+            </Link>
+            <Link
+              className="social-icon-link youtube"
+              to="/"
+              target="_blank"
+              area-label="Youtube"
+            >
+              <i className="fab fa-youtube"></i>
+            </Link>
+            <Link
+              className="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              area-label="Twitter"
+            >
+              <i className="fab fa-twitter"></i>
+            </Link>
+            <Link
+              className="social-icon-link linkedin"
+              to="/"
+              target="_blank"
+              area-label="Linkedin"
+            >
+              <i className="fab fa-linkedin"></i>
+            </Link>
+          </div>
         </div>
       </section>
     </div>
